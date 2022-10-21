19a98d72a0342dd8dfd7671ce5b8883a692cd8e4
Santa <avotina@protonmail.com>
2022-10-21 11:36:48 +0300

    rendering

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 import { menuArray } from "/data.js";
 
+function renderItems() {
+    let itemsHtml = ""
+    menuArray.forEach(function(item) {
+        itemsHtml += `
+        <span class="emoji">${item.emoji}</span>
+            <div class="item-info">
+                <span class="title">${item.name}</span>
+                <span class="ingredients">${item.ingredients}</span>
+                <span class="price">$${item.price}</span>
+            </div>
+            <button class="order-btn">+</button>
+        `
+    })
+    document.getElementsById("item-container").innerHTML = itemsHtml
+}
+
+renderItems()
