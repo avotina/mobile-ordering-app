e330fa98236acb407e27a64fab02d960ad48b536
Santa <avotina@protonmail.com>
2022-10-22 21:36:44 +0300

    renew

--- index.js
+++ index.js
@@ -1,19 +1,2 @@
 import { menuArray } from "/data.js";
 
-function renderItems() {
-    let itemsHtml = ""
-    menuArray.forEach(function(item) {
-        itemsHtml += `
-        <span class="emoji">${item.emoji}</span>
-            <div class="item-info">
-                <span class="title">${item.name}</span>
-                <span class="ingredients">${item.ingredients}</span>
-                <span class="price">$${item.price}</span>
-            </div>
-            <button class="order-btn">+</button>
-        `
-    })
-    document.getElementsById("item-container").innerHTML = itemsHtml
-}
-
-renderItems()
