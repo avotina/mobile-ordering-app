a6f6bf080802f23e11cb791745c4be92f56380c1
Santa <avotina@protonmail.com>
2022-10-26 23:58:24 +0300

    function

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import { menuArray } from "/data.js";
 
+function render() {
+    let itemHtml = ""
+    menuArray.forEach(function(item) {
+        itemHtml += `
+        <p>${item.name}</p>
+        `
+    })
+
+    document.getElementById("items-info").innerHTML = itemHtml
+}
+
+render()
